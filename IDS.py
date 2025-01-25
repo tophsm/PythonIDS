e6ae28474b9af7bc361af35bec914dc7fa41ee1c
tophsm <67989403+tophsm@users.noreply.github.com>
2025-01-24 22:45:32 -0800

    Update IDS.py

--- IDS.py
+++ IDS.py
@@ -14,3 +14,20 @@ class PacketCapture:
   def __init__ (self):
     self.packet_queue = queue.Queue()
     self.stop_capture = threading.Event()
+
+  def packet_callback(self, packet):
+    if IP in packet and TCP in packet:
+      self.packet_queue.put(packet)
+
+  def start_Capture(self, interface:"eth0"):
+    def capture_thread():
+      sniff(iface=interface,
+            prn=self.packet_callback,
+            store=0,
+            stop_filter=lambda_:self.stop_capture.is_set())
+      self.capture_thread = threading.Thread(target=capture_thread)
+      self.cature_thread.start()
+
+  def stop(self):
+    self.stop_capture.set()
+    self.capture_thread.join()
